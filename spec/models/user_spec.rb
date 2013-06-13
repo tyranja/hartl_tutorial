150d1a04a1e55fd72507b2be0dae56cd929407d3
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-06-13 22:24:28 +0200

    add test for downcase

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -70,6 +70,16 @@ describe User do
 		end
 	end
 
+	describe "email address with mixed case" do
+		let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+		it "should be saved as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			@user.reload.email.should == mixed_case_email.downcase
+		end
+	end
+
 	describe "when email address is already taken" do
 		before do
 			user_with_same_email = @user.dup
@@ -103,13 +113,13 @@ describe User do
 			it { should == found_user.authenticate(@user.password) }
 			specify { found_user.should be_true }
 		end
+	end
 
-		describe "with invalid password" do
-			let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+	describe "with invalid password" do
+		let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
-			it { should_not == user_for_invalid_password }
-			specify { user_for_invalid_password.should be_false }
-		end
+		it { should_not == user_for_invalid_password }
+		specify { user_for_invalid_password.should be_false }
 	end
 end 
 
