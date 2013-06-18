937515b7de41b36db8f710dcf2fedc802272ca8a
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-06-18 22:20:18 +0200

    add error and success message test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -37,6 +37,14 @@ describe "User pages" do
         initial.should == final
         # expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+        it { should have_content('contains') }
+      end
     end
 
     describe "with valid information" do
@@ -49,6 +57,12 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_content('Welcome') }
       end
     end
   end
