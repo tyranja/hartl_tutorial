c4e1d11d9d293658b536b836a3db6233292a84da
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-06-20 11:31:28 +0200

    modify create action in session contreoller for invalid sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # sign the user in and redirect to the user's show page
+    else
+      flash.now[:error] = 'Invalid email/password combination' 
+      render 'new'
+      # Create an message and re-render the singin form
+    end
   end
 
   def destroy
