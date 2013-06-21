1734cf5ce9769300a6184fa26f2f6a193ce0919e
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-06-21 13:40:12 +0200

    add a new test function in utilities.rb

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -16,7 +16,7 @@ describe "Authentication" do
     before { visit signin_path }
 
     describe "with invalid information" do
-      before { click_button "Sign in" }
+      before { invalid_signin }
 
       it { should have_selector('title', text: 'Sign in') }
       it { should have_error_meassage('Invalid') }
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,6 +6,18 @@ def valid_signin(user)
   click_button "Sign in"
 end
 
+def invalid_signin
+  click_button "Sign in"
+end
+
+def valid_signup(user) #doesnt work
+  fill_in "Name", with: user.name
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
+  fill_in "Confirmation", with: user.password_confirmation
+end
+
+
 RSpec::Matchers.define :have_error_meassage do |message|
   match do |page|
     page.should have_selector('div.alert.alert-error', text: message)
