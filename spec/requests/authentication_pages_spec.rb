2dd8d4b1f21f3aea33ed64e30ba148aff57dcf83
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-06-27 13:18:20 +0200

    admin can delete user

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -102,7 +102,18 @@ describe "Authentication" do
         before { put user_path(wrong_user) }
         specify { response.should redirect_to(root_path) }
       end
-      
+    end
+
+    describe "for non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) {FactoryGirl.create(:user) }
+
+      before { sign_in non_admin }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }
+      end
     end
   end
 end
