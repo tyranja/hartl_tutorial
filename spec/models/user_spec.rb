cab3816e41afb27ea806f0a8aedc8ee28fc8b1a3
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-07-16 18:30:41 +0200

    order the microposts in DESC

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -155,5 +155,19 @@ describe User do
 		end
 	end
 
+	describe "microposts associations" do
+		
+		before{ @user.save }
+		let!(:older_micropost) do
+			FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+		end
+		let!(:newer_micropost) do
+			FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+		end
+
+		it "should have the right microposts in the right order" do
+			@user.microposts.should == [newer_micropost, older_micropost]
+		end
+	end
 end 
 
